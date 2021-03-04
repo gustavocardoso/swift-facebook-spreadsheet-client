a4df65b05297561806ec99322d11de9eed12f7e0
Gustavo Cardoso <gustavocardoso@gmail.com>
2021-03-04 03:19:31 -0800

    Change limit options

--- src/App.js
+++ src/App.js
@@ -79,9 +79,6 @@ function App() {
 
           <label htmlFor='limit'>Limit</label>
           <select onChange={handleLimit} name='limit' id='limit'>
-            <option value='10'>10</option>
-            <option value='25'>25</option>
-            <option value='50'>50</option>
             <option value='100'>100</option>
             <option value='150'>150</option>
             <option value='200'>200</option>
